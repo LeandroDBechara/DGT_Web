a02a9c00d970f2b72f0f83374786eb023d1a7a55
leodbechara <leodbechara@gmail.com>
2024-09-21 21:34:10 +0000

    para atras

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://LeandroDBechara.github.io/DGT_Web",
 })
