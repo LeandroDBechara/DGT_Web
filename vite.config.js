177a286ed0406ef5930c4f9e51140b604947f04f
leodbechara <leodbechara@gmail.com>
2024-09-21 22:08:34 +0000

    dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"http://LeandroDBechara.github.io/dgt-web/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        admin: 'admin.html',
+        discapacidad: 'discapacidad.html',
+        mision: 'mision.html',
+        multas: 'multas.html',
+        normativas: 'normativas.html',
+        recorridos: 'recorridos.html',
+        resoluciones: 'resoluciones.html',
+        zafra: 'zafra.html'
+      }
+    }
+  }
 })
