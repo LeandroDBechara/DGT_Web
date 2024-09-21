cd6aa45349abf57fd3d6cfc1735fd58eac75fd2e
leodbechara <leodbechara@gmail.com>
2024-09-21 22:22:20 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://LeandroDBechara.github.io/dgt-web/",
+  base:"http://LeandroDBechara.github.io/dgt-web",
   build: {
     rollupOptions: {
       input: {
