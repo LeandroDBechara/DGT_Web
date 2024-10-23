9ec65834520bc7f32d501b206e0aced919adf2e1
leodbechara <leodbechara@gmail.com>
2024-10-23 19:43:49 +0000

    Updates

---
+++ assets/normativas-CH5nJfEu.js
@@ -0,0 +1,1 @@
+import{c as r,j as t,R as o,T as e,B as s}from"./ButtomBar-BlCFZZyj.js";import{D as i}from"./normativa-cI0R6-9A.js";import{a}from"./DocContext-BPXV144j.js";r.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsxs(a,{children:[t.jsx(e,{}),t.jsx("main",{children:t.jsx("div",{style:{margin:"20px"},children:t.jsx(i,{tipo:0})})}),t.jsx(s,{})]})}));
