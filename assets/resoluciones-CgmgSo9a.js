e02ba678d766a6832473e081168cc8fbdd14277a
leodbechara <leodbechara@gmail.com>
2024-10-26 12:50:47 +0000

    Updates

--- assets/resoluciones-CgmgSo9a.js
+++
@@ -1,1 +0,0 @@
-import{c as r,j as t,R as o,T as e,B as s}from"./ButtomBar-BlCFZZyj.js";import{D as i}from"./normativa-BKg2wni-.js";import{a}from"./DocContext-D3Q9RK-J.js";r.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsxs(a,{children:[t.jsx(e,{}),t.jsx("main",{children:t.jsx("div",{style:{margin:"20px"},children:t.jsx(i,{tipo:1})})}),t.jsx(s,{})]})}));
